8f512c1d2d97106051ee1673701acf4a57a9c04f
dfigalo1 <daianafigalo@gmail.com>
2019-06-11 21:09:50 -0300

    archivos

---
+++ Js/sale.js
@@ -0,0 +1,25 @@
+var local = {
+    seller: ["Ada", "Grace", "Hedy", "Sheryl"],
+
+    sale: [
+        { date: new Date(2019, 1, 4), sellingName: "Grace", components: ["Monitor GPRS 3000", "Motherboard ASUS 1500"] },
+        { date: new Date(2019, 0, 1), sellingName: "Ada", components: ["Monitor GPRS 3000", "Motherboard ASUS 1500"] },
+        { date: new Date(2019, 0, 2), sellingName: "Grace", components: ["Monitor ASC 543", "Motherboard MZI"] },
+        { date: new Date(2019, 0, 10), sellingName: "Ada", components: ["Monitor ASC 543", "Motherboard ASUS 1200"] },
+        { date: new Date(2019, 0, 12), sellingName: "Grace", components: ["Monitor GPRS 3000", "Motherboard ASUS 1200"] }
+    ],
+
+    prices: [
+        { component: "Monitor GPRS 3000", price: 200 },
+        { component: "Motherboard ASUS 1500", price: 120 },
+        { component: "Monitor ASC 543", price: 250 },
+        { component: "Motherboard ASUS 1200", price: 100 },
+        { component: "Motherboard MZI", price: 30 },
+        { component: "HDD Toyiva", price: 90 },
+        { component: "HDD Wezter Dishital", price: 75 },
+        { component: "RAM Quinston", price: 110 },
+        { component: "RAM Quinston Fury", price: 230 }
+      ],
+
+    offices: ['Centro', 'Caballito'],  
+    };
